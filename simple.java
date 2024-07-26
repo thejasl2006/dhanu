613a6533fc2c0763a2364a2107bfa4d971fbe843
thejasl2006 <thejasl6706@gmail.com>
2024-07-26 11:56:42 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -3,6 +3,6 @@ public class simple
 	public static void main (String []args)
 	{
 		system.out.println("welcome");
-		system.out.println("to tumkuru,gubbi");
+		system.out.println("to tumkuru,gubbi,mysore");
 	}
 }
