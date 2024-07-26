50d9508b1ac2fbb0b5b5ead09bc125a414ee8bd2
tanu <tanu123@gmail.com>
2024-07-26 11:50:46 +0530

    change3

--- simple.java
+++ simple.java
@@ -3,6 +3,6 @@ public class simple
 	public static void main (String []args)
 	{
 		system.out.println("welcome");
-		system.out.println("to tumkuru");
+		system.out.println("to tumkuru,gubbi");
 	}
 }
